d3b5c7f9bb320489e1fa6f7bccb8008b263fc5e0
AndyBoyee <n.andyvue@outlook.com>
2022-09-29 20:33:32 -0500

    This is dangerous; don't commit directly to main

--- server/models/user.js
+++ server/models/user.js
@@ -6,21 +6,23 @@ const Post = require('./post')
 const userSchema = new Schema(
     {
         username: {
-            type: String, 
+            type: String,
             required: true,
             unique: true,
         },
         email: {
-            type: String, 
+            type: String,
             required: true,
             unique: true,
             match: [/.+@.+\..+/, 'Must use a valid email address'],
         },
         password: {
-            type: String, 
+            type: String,
             required: true,
             minLength: 7
-        },
+        }
+    },
+    {
         toJSON: {
             virtuals: true,
         },
