0008c46469c2514e58bb26714b3e46d38de9c4a0
tlor0026 <tlor0026@gmail.com>
2022-10-04 22:39:47 -0500

    adding back folder

---
+++ client/src/components/resturantList/resturantList.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+import '../../App.css'
+
+const linkStyle = {
+  color: "#66ccff",
+};
+
+const titleStyle = {
+  backgroundColor: "#66ccff",
+  padding: "20px",
+};
+
+const resturantStyle = {
+  padding: "20px",
+  backgroundColor: '#212429',
+  justifyContent: 'Center'
+};
+
+const imgStyle = {
+  height: "300px",
+  width: "500px",
+};
+
+const ResturantList = ({ 
+  resturant,
+  name,
+  image,
+  description
+ }) => {
+  if (!resturant.length) {
+    return <h3>Resturants</h3>;
+  }
+
+    return (
+      <section className="resturant">
+
+        <h1 style={titleStyle}>Resturants</h1>
+        {resturant.map((resturant) => (
+          <div className="row" key={resturant._id} style={resturantStyle}>
+          <div className="col-sm-8">
+            <div className="card text-center">
+              <div className="card-body">
+                <h5 className="card-title">{resturant.name}</h5>
+                <img className="img1" src={resturant.image}></img>
+                <p className="card-text">
+                  {resturant.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        ))}
+      </section>
+    )
+};
+
+export default ResturantList;
